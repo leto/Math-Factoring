c8551ec19626ae64ef2dc63d36be6e2a83639af2
Duke Leto <jonathan@leto.net>
2009-06-21 23:28:49 -0700

    Initial commit

---
+++ lib/Math/Factoring.pm
@@ -0,0 +1,79 @@
+package Math::Factoring;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+Math::Factoring - Advanced Factoring Algorithms
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+    use Math::Factoring;
+
+=head1 AUTHOR
+
+Jonathan Leto, C<< <jonathan at leto.net> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-math-factoring at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Math::Factoring>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Math::Factoring
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Math::Factoring>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Math::Factoring>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Math::Factoring>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Math::Factoring>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 Jonathan Leto, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+
+=cut
+
+1; # End of Math::Factoring
